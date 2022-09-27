314c7242e8a4102ee80890cf4b0d5ed6e43ab0bd
triveniksamo <114400850+trivenikasmo@users.noreply.github.com>
2022-09-27 12:33:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.dataframe(fruits_to_show')
+streamlit.dataframe(fruits_to_show)
